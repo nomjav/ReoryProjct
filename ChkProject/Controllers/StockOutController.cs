fd2c9c9ace12aa845141b44a9c98b2f4a8815d15
Eitsham <eitsham.03@gmail.com>
2018-09-30 17:55:05 +0500

    Eitsham Stage

---
+++ ChkProject/Controllers/StockOutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChkProject.Controllers
+{
+    public class StockOutController : Controller
+    {
+        // GET: StockOut
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
