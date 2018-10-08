d568abcc15aa49df29259dcf9b80d5cd2dc9bd5b
Eitsham <eitsham.03@gmail.com>
2018-10-08 23:52:44 +0500

    material changes mostly

--- ChkProject/Controllers/AccountController.cs
+++ ChkProject/Controllers/AccountController.cs
@@ -12,7 +12,7 @@ using ChkProject.Models;
 
 namespace ChkProject.Controllers
 {
-   // [Authorize]
+    [Authorize]
     public class AccountController : Controller
     {
         private ApplicationSignInManager _signInManager;
--- ChkProject/Controllers/MaterialController.cs
+++ ChkProject/Controllers/MaterialController.cs
@@ -1,17 +1,134 @@
-﻿using System;
+﻿using Chakwal.Data.Data;
+using Chakwal.Data.Repository;
+using ChkProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Chakwal.Data.MemberShip;
 
 namespace ChkProject.Controllers
 {
     public class MaterialController : Controller
     {
+        private UnitOfWork _unitOfWork = new UnitOfWork();
         // GET: Item
         public ActionResult Index()
         {
-            return View();
+            ItemModel _ItemModel = new ItemModel();
+            var material = _unitOfWork.ItemRepository.Get(x => x.IsDeleted == false);
+            foreach (var item in material)
+            {
+                ItemModel _material = new ItemModel();
+                _material.ItemId = item.ItemId;
+                _material.ItemName = item.ItemName;
+                _material.UnitPrice = item.UnitPrice;
+                _material.QuantityPresent = item.QuantityPresent;
+                _material.MeasureUnit = item.MeasureUnit;
+                // _material.Description = item.Description;
+                _material.CreatedBy = item.CreatedBy;
+                _material.CreatedDate = item.CreatedDate;
+
+
+                _ItemModel.ItemList.Add(_material);
+            }
+            return View(_ItemModel);
         }
+
+
+        [HttpPost]
+        public JsonResult CheckDuplication(string ItemName)
+        {
+            var item = _unitOfWork.ItemRepository.GetSingle(t => t.ItemName == ItemName);
+            return Json(item, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public JsonResult UpdateMaterial(ItemModel model)
+        {
+            try
+            {
+                var _item = _unitOfWork.ItemRepository.GetSingle(t => t.ItemId == model.ItemId);
+                var _user = _unitOfWork.UserRepository.GetSingle(t => t.UserName == User.Identity.Name);
+                if (_user != null)
+                {
+                    _item.ModifiedBy = _user.Id;
+                }
+
+                if (model.IsDeleted == true)
+                {
+                    _item.IsDeleted = true;
+                    _item.DeletedBy = _user.Id;
+                    _item.DeletedDate = DateTime.Now;
+
+                }
+                else
+                {
+                    _item.ItemName = model.ItemName;
+                    _item.UnitPrice = model.UnitPrice;
+                    _item.QuantityPresent = model.QuantityPresent;
+                   // _item.Description = model.Description;
+                    _item.ModifiedDate = DateTime.Now;
+                }
+
+
+                _unitOfWork.ItemRepository.Update(_item);
+                _unitOfWork.Save();
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            catch
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult AddMaterial(ItemModel model)
+
+        {
+
+            Item _Item = new Item();
+            _Item.ItemName = model.ItemName;
+            _Item.UnitPrice = model.UnitPrice;
+            _Item.QuantityPresent = model.QuantityPresent;
+           //_Item.Description = model.Description;
+            _Item.CreatedDate = DateTime.Now;
+            var user = _unitOfWork.UserRepository.GetSingle(t => t.UserName == User.Identity.Name);
+            if (user != null)
+            {
+                _Item.CreatedBy = user.Id;
+            }
+            if (model.MeasureUnit == "1")
+            {
+                _Item.MeasureUnit = "Kg";
+            }
+            if (model.MeasureUnit == "2")
+            {
+                _Item.MeasureUnit = "Liter";
+            }
+            if (model.MeasureUnit == "3")
+            {
+                _Item.MeasureUnit = "Meter";
+            }
+            if (model.MeasureUnit == "4")
+            {
+                _Item.MeasureUnit = "Other";
+            }
+            else
+            {
+                _Item.MeasureUnit = "Other";
+            }
+
+
+            _unitOfWork.ItemRepository.Insert(_Item);
+            _unitOfWork.Save();
+            return RedirectToAction("Index");
+        }
+
+
+
     }
 }
